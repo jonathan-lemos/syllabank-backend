30114ca960d985ac4e6c905ea55b52495974c02f
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-04-15 22:20:37 -0400

    added site submodule. work mostly done

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
 import SQLServer from "./sql";
+import WebServer from "./web";
 
 const main = async () => {
-	const con = await SQLServer.create({ user: "root", password: "root" });
-	try {
-		await con.insert({ course: "COT3210", professor_first: "Egg", professor_last: "Man", time_begin: "13:00:00", time_end: "14:30:00", days: "TR", term: "Fall", year: 2018 });
-		await con.insert({ course: "COT3210", professor_first: "Red", professor_last: "Ferrari", time_begin: "14:00:00", time_end: "15:30:00", days: "TR", term: "Fall", year: 2018 });
-		let res = await con.select({ course: "COT3210" });
-		console.log(JSON.stringify(res) + "\n");
-		res = await con.select({ professor_first: "Red", professor_last: "Ferrari" });
-		console.log(JSON.stringify(res) + "\n");
-		await con.delete({course: "COT3210"});
-
-		await con.insert({ course: "COT3210", professor_first: "Egg", professor_last: "Man", time_begin: "13:00:00", time_end: "14:30:00", days: "TR", term: "Fall", year: 2018 });
-		await con.insert({ course: "COT3210", professor_first: "Red", professor_last: "Ferrari", time_begin: "14:00:00", time_end: "15:30:00", days: "TR", term: "Fall", year: 2018 });
-		res = await con.select({ course: "COT3210" });
-		console.log(JSON.stringify(res) + "\n");
-
-		await con.end();
-	}
-	catch (e) {
-		await con.end();
-		throw e;
-	}
+	const con = await SQLServer.create({user: "root", password: "root"});
+	const web = new WebServer(con, 3000);
+	await web.listen().then(() => console.log("Express listening on port 80"));
 };
 
 main()
--- web.js
+++ web.js
@@ -1,10 +1,78 @@
-import express;
-import path;
+import express from "express";
+import path from "path";
 
 export default class WebServer {
-	static sendError(res, msg) {
+	static _senderror(res, msg) {
 		res.status(400).send(msg);
 	}
 
-	constructor(sqlcon, port = 80, baseDir = path.join(__dirname, "../"))
+	constructor(sqlcon, port = 80, baseDir = path.join(__dirname, "/site"), defaultFile = "index.html") {
+		if (typeof port !== "number") {
+			throw new Error("Port must be a number");
+		}
+
+		if (typeof baseDir !== "string") {
+			throw new Error("baseDir must be a string");
+		}
+
+		if (typeof defaultFile !== "string") {
+			throw new Error("defaultFile must be a string");
+		}
+
+		this.nodeServer = null;
+		this.con = sqlcon;
+		this.port = port;
+		this.web = express();
+		this.web.use(express.static(baseDir));
+		this.web.get("/", (req, res) => res.sendFile(path.join(baseDir, defaultFile)));
+
+		this.web.get("/api/select", async (req, res) => {
+			try {
+				res.json(await this.con.select(req.query));
+			}
+			catch (e) {
+				WebServer._senderror(res, e);
+			}
+		});
+
+		this.web.get("/api/selectSylabi", async (req, res) => {
+			try {
+				res.json(await this.con.selectProfessors(req.query));
+			}
+			catch (e) {
+				WebServer._senderror(res, e);
+			}
+		});
+
+		this.web.get("/api/selectProfessors", async (req, res) => {
+			try {
+				res.json(await this.con.selectProfessors(req.query));
+			}
+			catch (e) {
+				WebServer._senderror(res, e);
+			}
+		});
+
+		this.web.get("/api/selectCourses", async (req, res) => {
+			try {
+				res.json(await this.con.selectProfessors(req.query));
+			}
+			catch (e) {
+				WebServer._senderror(res, e);
+			}
+		});
+	}
+
+	end() {
+		if (this.nodeServer !== null) {
+			this.nodeServer.close();
+		}
+		this.con.end();
+	}
+
+	async listen() {
+		return new Promise((resolve, reject) => {
+			this.nodeServer = this.web.listen(this.port, () => resolve());
+		})
+	}
 }
